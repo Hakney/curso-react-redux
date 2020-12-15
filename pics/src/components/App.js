cd252086375dffc7fe02c4c116a9d2c8cd220cc0
Hakney <hakneey@gmail.com>
2020-12-15 17:53:05 -0300

    Refatorar Axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import axios from 'axios';
+import instanceAxios from '../api/instanceAxios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term},
-            headers: {
-                Authorization: 'Client-ID 8vhzk7MP0ed65Plk7Xvr9LbHloMqtQnEW10jcQwsEH4' 
-            }
+    state = {images: []};
+
+     onSearchSubmit = async (term) => {
+        const response = await instanceAxios.get('/search/photos', {
+            params: { query: term}, 
         });
+
+       this.setState({ images: response.data.results });
     }
     
     render(){
         return(
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar pesquisar={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
             </div>
         );
     }
